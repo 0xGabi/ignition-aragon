6a424fcdde935d3021e911430a580ba4789f34d2
0xGabi <gabrielpk.18@gmail.com>
2023-07-28 18:20:37 -0300

    Initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,23 @@
+import { HardhatUserConfig } from "hardhat/config";
+
+import "@ignored/hardhat-ignition";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.4.24',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+    ],
+  },
+};
+
+export default config;
